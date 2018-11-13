b66e893a217c83adcec8e6207f6e16e8ae0162fc
Memnoc <smartdroidesign@gmail.com>
2018-11-13 08:52:59 +0000

    Handler implemented post() and postDelayed() implemented

--- app/src/main/java/com/smartdroidesign/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/smartdroidesign/stopwatch/StopwatchActivity.java
@@ -1,5 +1,6 @@
 package com.smartdroidesign.stopwatch;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,14 +38,20 @@ public class StopwatchActivity extends AppCompatActivity {
 
     private void runTimer() {
         final TextView timeView = findViewById(R.id.time_view);
-
-        int hours = seconds/3600;
-        int minutes = (seconds %3600)/60;
-        int secs = seconds%60;
-        String time = String.format(Locale.UK, "%d:%02d:%02d", hours, minutes, secs);
-        timeView.setText(time);
-        if (running) {
-            seconds++;
-        }
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds %3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.UK, "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
  }
